665a6224eded4a5ccf08e04bcbdd42d1da77002c
m6r <77928890+m1xb3r@users.noreply.github.com>
2024-11-07 11:15:28 +0100

    add debug flags to assembler script for NASM and LD compilation

--- assembler.sh
+++ assembler.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
 fileName="${1%%.*}" # remove .s extension
 
-nasm -f elf64 ${fileName}".s"
-ld ${fileName}".o" -o ${fileName}
-[ "$2" == "-g" ] && gdb -q ${fileName} || ./${fileName}
+if [ "$2" == "-g" ]; then
+    # Compile with debug info when -g is specified
+    nasm -f elf64 -F dwarf -g "${fileName}.s" -o "${fileName}.o"
+    ld -g "${fileName}.o" -o "${fileName}"
+    gdb -q "${fileName}"
+else
+    # Normal compilation without debug info
+    nasm -f elf64 "${fileName}.s" -o "${fileName}.o"
+    ld "${fileName}.o" -o "${fileName}"
+    ./"${fileName}"
+fi
